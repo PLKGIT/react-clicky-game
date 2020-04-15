53f0a4d22544f7770f238266c3841f4f0773032a
Lammie <plkelly@yahoo.com>
2020-04-15 00:18:58 -0700

    Basic react install.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Pam Kelly!</h1>
+    </div>
+  );
+}
+
+export default App;
