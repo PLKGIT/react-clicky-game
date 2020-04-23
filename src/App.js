c82e20b875dc4f1ce67ef79059dea8d618fc1d28
Lammie <plkelly@yahoo.com>
2020-04-22 17:33:11 -0700

    Added logic to handle clicks.

--- src/App.js
+++ src/App.js
@@ -17,78 +17,82 @@ class App extends Component {
   }
 
   componentDidMount() {
-    this.setState({ images: this.ShuffleImages(this.state.images) 
+    this.setState({
+      images: this.ShuffleImages(this.state.images)
     });
   }
 
+  HandleCorrectClick = userClicked => {
+    const { score, topScore } = this.state;
+    const scoreCount = score + 1;
+    const topScoreCount = Math.max(scoreCount, topScore);
+
+    this.setState({
+      images: this.shuffleImages(userClicked),
+      score: scoreCount,
+      topScore: topScoreCount
+    });
+
+  }
+
+  HandleRepeatClick = userClicked => {
+    this.setState({
+      images: this.resetImages(userClicked),
+      score: 0
+    });
+  }
+
+  ResetClicked = images => {
+    const resetClicked = images.map(item => ({ ...item, clicked: false }));
+    return this.shuffleImages(resetClicked);
+  };
+
+  ShuffleImages = images => {
+    let i = images.length - 1;
+    while (i > 0) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = images[i];
+      images[i] = images[j];
+      images[j] = temp;
+      i--;
+    };
+    return images;
+  };
+
   HandleCardClicked = event => {
     console.log(`Card ${event} clicked!`)
-  
-  
+
+
   }
 
-HandleRenderMessage() {
-  // Render appropriate Message
-  if (!this.state.error) {
-    this.setState({ message: "Guessed correctly!" })
-  } else {
-    this.setState({ message: "Guessed incorrectly!" })
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <NavBar
+          message={this.state.message}
+          score={this.state.score}
+          topScore={this.state.topScore}
+        />
+        <Header />
+
+        <Container>
+          {this.state.images.map(images => (
+            <Cards
+              id={images.id}
+              key={images.id}
+              image={images.image}
+              HandleCardClicked={this.HandleCardClicked}
+            />
+          ))}
+        </Container>
+
+        <Footer />
+      </div>
+    );
   }
-}
-
-HandleCompareScore = () => {
-  // If score greater than top score, add one to top score
-  // Shuffle cards
-  this.state.topScore < this.state.score ? this.setState({ topScore: this.state.topScore + 1 }) :
-    console.log('Top Score');
-  console.log(this.state.topScore);
-  this.HandleShuffle();
-}
-
-ResetClicked = images => {
-  const resetClicked = images.map(item=>({...item,clicked:false}));
-  return this.shuffleImages(resetClicked);
-  };
-
-ShuffleImages = images => {
-  let i = images.length - 1;
-  while (i > 0) {
-    const j = Math.floor(Math.random() * (i + 1));
-    const temp = images[i];
-    images[i] = images[j];
-    images[j] = temp;
-    i--;
-  };
-  return images;
-};
-
-
-render() {
-  return (
-    <div className="App">
-      <NavBar
-        message={this.state.message}
-        score={this.state.score}
-        topScore={this.state.topScore}
-      />
-      <Header />
-
-      <Container>
-        {this.state.images.map(images => (
-          <Cards
-            id={images.id}
-            key={images.id}
-            name={images.name}
-            image={images.image}
-            HandleCardClicked={this.HandleCardClicked}
-          />
-        ))}
-      </Container>
-
-      <Footer />
-    </div>
-  );
-}
 }
 
 export default App;
